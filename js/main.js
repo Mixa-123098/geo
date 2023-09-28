51f4e3725c76cc090e893d55dea689d1c0167fcb
Mixa-123098 <60194946+Mixa-123098@users.noreply.github.com>
2023-09-29 02:10:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert('hello')
+const parallaxBg = document.querySelector('#header');
+
+console.log(parallaxBg)
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    parallaxBg.style.transform = `translateY(${scrollY * 0.5}px)`;
+    
+});
