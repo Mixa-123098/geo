8bd2589e0e073c2889b33fb384e54fe25eead43c
Username14323 <140832209+Username14323@users.noreply.github.com>
2023-10-17 23:20:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ window.addEventListener('scroll', () => {
     parallaxBg.style.transform = `translateY(${scrollY * 0.5}px)`;
     
 });
+window.onload = function () {
+    document.querySelector('.cont_modal').className = "cont_modal";
+}
+var c = 0;
+function open_close() {
+    if (c % 2 == 0) {
+        document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";
+        c++;
+    } else {
+        document.querySelector('.cont_modal').className = "cont_modal";
+        c++;
+    }
+} 
